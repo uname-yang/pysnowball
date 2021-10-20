87ce072b04ce67b4eed0e2cde02caad8472b6a22
然Y <inie0722@gmail.com>
2021-10-20 20:30:50 +0800

    cube 采用独立host

--- pysnowball/utls.py
+++ pysnowball/utls.py
@@ -4,8 +4,9 @@ import json
 import pysnowball.cons as cons
 import pysnowball.token as token
 
-def fetch(url):
-    HEADERS = {'Accept': 'application/json',
+def fetch(url, host = "stock.xueqiu.com"):
+    HEADERS = {'Host': host,
+               'Accept': 'application/json',
                'Cookie': token.get_token(),
                'User-Agent': 'Xueqiu iPhone 11.8',
                'Accept-Language': 'zh-Hans-CN;q=1, ja-JP;q=0.9',
@@ -25,8 +26,9 @@ def fetch(url):
     return json.loads(response.content)
 
 
-def fetch_without_token(url):
-    HEADERS = {'Accept': 'application/json',
+def fetch_without_token(url, host = "stock.xueqiu.com"):
+    HEADERS = {'Host': host,
+               'Accept': 'application/json',
                'User-Agent': 'Xueqiu iPhone 11.8',
                'Accept-Language': 'zh-Hans-CN;q=1, ja-JP;q=0.9',
                'Accept-Encoding': 'br, gzip, deflate',
